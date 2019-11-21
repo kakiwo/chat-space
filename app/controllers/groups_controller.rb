dd85bee766cd19d1314e55c65c27245eb94e100b
kakiwo <kakiwo@hotmail.co.jp>
2019-11-21 16:34:49 +0900

    add-incremental-search

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
   
@@ -17,8 +19,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.new
-    @group.users << current_user
+    @group = Group.find(params[:id])
   end
 
   def update
